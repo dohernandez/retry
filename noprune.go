afc74029f18a8bcd8e6731647e8f0e0c4dfb0594
Darien Hernandez <dohernandez@gmail.com>
2024-01-05 00:38:56 +0100

    patch: Update dev pkg

--- noprune.go
+++ noprune.go
@@ -3,7 +3,4 @@
 
 package noprune
 
-import (
-	_ "github.com/dohernandez/dev" // Include CI/Dev scripts to project.
-	_ "github.com/dohernandez/storage"
-)
+import _ "github.com/dohernandez/dev" // Include CI/Dev scripts to project.
